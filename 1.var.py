b671d4c2dd3a69d91250e823181afda6946a04fd
Vladmer27 <vlad_2421996@mail.ru>
2021-05-27 20:09:08 +0900

    created new file named as 1.var.py

---
+++ 1.var.py
@@ -0,0 +1,48 @@
+# переменные
+my_var_1 = 100
+my_var_2 = 200
+c = my_var_1 + my_var_2
+
+#комментирование - ctrl + /
+# print(c)
+
+PI = 3.14
+#*** Data types ***
+#Int - целое float - натуральное число 
+_int = 100000
+_float = 500.001
+my_str = "hello 'there'"
+# print(my_str)
+# print(_float)
+
+# булевы типы данных (boolean, bool)
+# true/false 
+my_bool = True #true = логическая единица, false = логический ноль 
+# *** способ форматирования под названием "f - строка"
+# способ форматирования - конкатинация 
+z = "Hello"
+r = " "
+q = ", world!"
+
+s = z + r + q
+# print(s)
+res = f"the first word: {z} the second word {r} the third word{q}"
+# print(res)
+
+# ***Арифметические операции***
+var_1 = 10
+var_2 = 2
+
+# операция сложения 
+print(var_1 + var_2)
+
+# операция вычитания
+print(var_1 - var_2)
+
+# операция произведения 
+print(var_1 * var_2)
+
+# операция простого деления(частного). 
+# Результат будет всегда натуральным числом (float)
+# При этом без округления 
+print(var_1 // var_2)
