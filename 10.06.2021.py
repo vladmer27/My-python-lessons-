83bcb6b566aaad2906a0b020bfd2205e62a8ced7
Vladmer27 <vlad_2421996@mail.ru>
2021-06-10 20:05:21 +0900

    files are uploaded

--- 10.06.2021.py
+++ 10.06.2021.py
@@ -5,6 +5,7 @@
 # print(len(a))
 # print(2 in a)
 
+
 # # list
 # b = [1,2,3,4,5,6]
 # print(b)
@@ -14,12 +15,50 @@
 # print(b.count(2))
 
 
-# # set
+# set
 # c = {1,2,3,4,5,6}
 # print(c)
+# c.add(123)
+
 # print(type(c))
 # print(len(c))
 # print(2 in c)
+c = {10,20,30}
+# добавление множества
+c.add(30)
+print(c)
+c.remove(20)
+# c.remove(123)
+# print(c)
+
+# метод удаления без ошибок 
+c.discard(10)
+
+# Дано два множества 
+W = {"A", 'B', 'C', 'D', 'E'}
+Z = {'B', 'C', 'Q'}
+
+# объединить 
+# F = W.union(Z)
+F = W | Z
+# print(F)
+
+# КАК КРУГИ ЭЙЛЕРА
+# пересечение 
+F = W.intersection(Z)
+# короткая запись пересечение 
+F = W&Z
+print(F)
+
+# разность 
+F = W.difference(Z)
+F = Z.difference(W)
+F = W.symmetric_difference(Z)
+# короткая запись symmetric difference 
+F = W - Z
+# СРС эксперименты с оставшимися методами 
+# print(F)
+
 
 # # dict
 # d = {'a': 1, 'b': 2, "c":3, 'd':4, 'e':5}
@@ -32,13 +71,17 @@
 # print(('a', 1) in d.items())
 
 
-# string 
-e = "hello 1 world"
-print(e)
-print(len(e))
-print('e' in e)
-print("hello" in "hellow world")
-print(e.count('h'))
-print(e.index('h'))
+# # string 
+# e = "hello 1 world"
+# print(e)
+# # print(len(e))
+# # print('e' in e)
+# # print("hello" in "hellow world")
+# # print(e.count('h'))
+# # print(e.index('h'))
+# print(e[-1])
+# e1= e[2:]
+# print(e1)
+
 
 
---
+++ loops.py
@@ -0,0 +1,43 @@
+# * * * ЦИКЛЫ * * *
+
+# оператор цикла While
+
+# БЕСКОНЕЧНЫЙ ЦИКЛ
+# while True:
+#     print("hello")
+
+# I = 1
+# while I <= 2: 
+#     print(I**2)
+#     I = I +1
+
+# g = 10
+# while g > 0:
+#     g -= 1
+#     print (f"hello {g}")
+
+# инструкция прерывания цикла по доп. условию
+# g = 10
+# while g > 0:
+#     print (f"hello {g}")
+#     if g == 5:
+#         break
+#     g -= 1
+    
+# *** Оператор цикла for *** 
+# 1. Читает значение itaruable (list, tuple, string) по порядку
+# 2. значение присваивает в свою переменную 
+# 3. Выполняет блок кода своего тела 
+
+# my_list = [10,20,30,40,50,60,70]
+# my_list.remove(10)
+# print(my_list)
+# for i in my_list[::-1]:
+#     print(i)
+
+# генератор списка 
+# создание листа чисел в диапозоне от 10 до 100 с шагом 10
+num_list = [num *2 for num in range(10, 100, 10)]
+print(num_list)
+
+
