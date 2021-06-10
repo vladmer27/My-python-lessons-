d2cfd1b6231ae4a054bb0ee86c266a3151c5cd92
Vladmer27 <vlad_2421996@mail.ru>
2021-06-10 18:08:38 +0900

    file is uploaded

---
+++ 10.06.2021.py
@@ -0,0 +1,44 @@
+# tuple
+# a = (1,2,3,4,5,6)
+# print(a)
+# print(type(a))
+# print(len(a))
+# print(2 in a)
+
+# # list
+# b = [1,2,3,4,5,6]
+# print(b)
+# print(type(b))
+# print(len(b))
+# print(2 in b) 
+# print(b.count(2))
+
+
+# # set
+# c = {1,2,3,4,5,6}
+# print(c)
+# print(type(c))
+# print(len(c))
+# print(2 in c)
+
+# # dict
+# d = {'a': 1, 'b': 2, "c":3, 'd':4, 'e':5}
+# print(d)
+# print(type(d))
+# print(len(d))
+# print('a' in d)
+# print('a' in d.keys())
+# print(2 in d.values())
+# print(('a', 1) in d.items())
+
+
+# string 
+e = "hello 1 world"
+print(e)
+print(len(e))
+print('e' in e)
+print("hello" in "hellow world")
+print(e.count('h'))
+print(e.index('h'))
+
+
