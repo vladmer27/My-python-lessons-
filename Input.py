9ac01aaaf6a2693559717490eeba062d786c9db8
Vladmer27 <vlad_2421996@mail.ru>
2021-06-14 00:10:49 +0900

    files are uploaded

--- Input.py
+++ Input.py
@@ -1,4 +1,15 @@
-print('What is your name?')
-name = input("enter your name:")
-print("Hello there " + name + '!')
+# 13.06.2021
+# print('What is your name?')
+# name = input("enter your name:")
+# print("Hello there " + name + '!')
 
+# a = input("enter ypur value: ")
+# b = input("enter your value: ")
+# s = a + b
+# print(s)
+
+a =int(input())
+b = int(input())
+s = a * b 
+print(s)
+print(type(s))
