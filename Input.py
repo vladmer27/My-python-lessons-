2ef07bfef204e69a606a295221c1ce28e4e2559d
Vladmer27 <vlad_2421996@mail.ru>
2021-06-15 22:08:23 +0900

    files are uploaded

--- Input.py
+++ Input.py
@@ -28,12 +28,41 @@
 # print (c)
 # print (type(c))
 
-n = int (input())
-k = int (input())
-# деление с остатком 
-print (n/k)
-# деление без остатка
-print (n//k)
-# делание с остатком 
-print (n%k)
+# n = int (input())
+# k = int (input())
+# # деление с остатком 
+# print (n/k)
+# # деление без остатка
+# print (n//k)
+# # остаток от деления
+# print (n%k)
+
+# часы 
+# n = int(input())
+# hours = n%60*24//60
+# minutes = n%60
+# print (hours, minutes)
+
+# name = input("what is your name?")
+# print('Hello there, ' + name + '!')
+
+# *** the next and previous
+# number = int(input())
+# previous_number = number - 1
+# next_number = number + 1
+# print (f"The next number for the number {number} is {next_number}")
+# print (f"The previous number for the number {number} is {previous_number}")
+
+# ***The number of desks in a classroom 
+# a = float(input())
+# b = float(input())
+# c = float(input())
+# print (a//2 + b//2 + c//2 + a%2 + b%2 + c%2)
+
+# *** Длинна шнурка***
+a = int(input())
+b = int(input())
+l = int(input())
+n = int(input())
+print((2*l) + (2*n-1)*a +2*(n-1)*b)
 
